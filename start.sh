df787e8e9e36141f5478610ec17e5d458abdeda2
Samuel Allen <dehuszar@gmail.com>
2025-07-14 22:52:54 -0400

    django, be GONE!

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 docker run --name datastar-events-demo \
+  -v $(pwd):/app \
+  -v /app/.venv \
   --net=host \
   --rm \
   datastar-events-demo 
 
 
-  # -v $(pwd)/jinja:/app/jinja \
