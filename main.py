709a012ffe940456da8b8a12b29cc2f2fb600f49
Samuel Allen <dehuszar@gmail.com>
2026-06-12 15:22:37 -0400

    bumping header content out into its own template so navigation state can be set by backend

--- main.py
+++ main.py
@@ -14,22 +14,32 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Set up Jinja2 templates directory
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.get("/about/backend", response_class=HTMLResponse)
 async def backend(request: Request):
     return templates.TemplateResponse("about/backend.html", {"request": request})
 
+
 @app.get("/about/datastar", response_class=HTMLResponse)
 async def backend(request: Request):
     return templates.TemplateResponse("about/datastar.html", {"request": request})
+
 
 @app.get("/about/events", response_class=HTMLResponse)
 async def backend(request: Request):
     return templates.TemplateResponse("about/events.html", {"request": request})
 
+
 @app.get("/about/uswds", response_class=HTMLResponse)
 async def backend(request: Request):
     return templates.TemplateResponse("about/uswds.html", {"request": request})
+
+
+@app.get("/demos/story-time", response_class=HTMLResponse)
+async def backend(request: Request):
+    return templates.TemplateResponse("demos/story-time.html", {"request": request})
