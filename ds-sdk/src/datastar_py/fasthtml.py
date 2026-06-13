105779db2e7d8629bf363e337394c71e4ec805bb
Samuel Allen <dehuszar@gmail.com>
2026-06-12 20:43:43 -0400

    stripping out old sdk code

--- ds-sdk/src/datastar_py/fasthtml.py
+++
@@ -1,9 +0,0 @@
-from .sse import SSE_HEADERS, ServerSentEventGenerator
-from .starlette import DatastarResponse, read_signals
-
-__all__ = [
-    "SSE_HEADERS",
-    "DatastarResponse",
-    "ServerSentEventGenerator",
-    "read_signals",
-]
